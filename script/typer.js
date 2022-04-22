73911803e52e4b393cdce86f6efa3ec03c5602ca
OverKnown1 <francyoppureno@gmail.com>
2022-04-22 22:40:33 +0200

    Update typer.js

--- script/typer.js
+++ script/typer.js
@@ -2,10 +2,30 @@ var app = document.getElementById('app');
 
 
 var typewriter = new Typewriter(app, {
-    delay: 75,
+    delay: 95,
 });
       
   typewriter
     .pauseFor(800)
-    .typeString("Raptor's lil lounge, also website for coding self training.")
+    .typeString("Raptor's lil lounge")
+    .pauseFor(170)
+    .typeString('.')
+    .pauseFor(1650)
+    .deleteChars(1)
+    .pauseFor(145)
+    .typeString(',')
+    .pauseFor(550)
+    .typeString(" also website for self learning coding")
+    .pauseFor(170)
+    .typeString('.')
+    .pauseFor(7500)
+    .deleteChars(1)
+    .pauseFor(145)
+    .typeString(" ")
+    .pauseFor(300)
+    .typeString(":")
+    .pauseFor(350)
+    .typeString(")")
+    .pauseFor(260)
+    .typeString(".")
     .start();
